e266a9b342e0ebe3f84708f753c04163ff752a2f
trohit7 <rohit.thottempudi7@gmail.com>
2022-07-02 15:23:26 +0530

    changed baseurl

--- src/App.vue
+++ src/App.vue
@@ -24,8 +24,8 @@ export default {
   data() {
     return {
       
-      //baseURL: "http://159.89.171.94:8085/",
-      baseURL: "http://localhost:8085/",
+      baseURL: "http://159.89.171.94:8085/",
+      // baseURL: "http://localhost:8085/",
       products: [],
       categories: [],
       key: 0,
