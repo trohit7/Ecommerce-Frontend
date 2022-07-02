b8435110adf57871aa0dc9e6e6d5edb52aba084e
trohit7 <rohit.thottempudi7@gmail.com>
2022-07-02 19:42:00 +0530

    back to normal config.js

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,5 @@
 // vue.config.js
 module.exports = {
-
-    pages: {
-        // This is our main page which refers to the main Javascript file
-        index: "src/main.js", 
-        // Next, we list all other pages in the application, 
-        // and let all of them refer to the main Javascript file as well.
-        other_page: "src/views/signin.vue",
-      },
     // options...
     devServer: {
         disableHostCheck: true
